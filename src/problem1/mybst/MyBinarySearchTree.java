71c14aab282d8e248c55030df674192764bd6ec6
naman <naman.gupta_cs18@gla.ac.in>
2020-03-27 19:47:31 +0530

    generate getter and setter methods for that field

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -11,4 +11,12 @@ import problem1.node.TreeNode;
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
     private TreeNode root;
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public void setRoot(TreeNode root) {
+        this.root = root;
+    }
 }
