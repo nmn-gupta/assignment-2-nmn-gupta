bdc0a139cd6563aa621a6dd4f2ef02c13d2fe61b
naman <naman.gupta_cs18@gla.ac.in>
2020-03-27 21:04:56 +0530

    generate getter and setter methods for front field

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -11,4 +11,11 @@ import problem3.node.Node;
 public class MyPriorityQueue {
     private Node front;
 
+    public Node getFront() {
+        return front;
+    }
+
+    public void setFront(Node front) {
+        this.front = front;
+    }
 }
