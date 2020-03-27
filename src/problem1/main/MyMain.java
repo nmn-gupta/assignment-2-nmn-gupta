0e89f0fde20e6c9979c8b10d492e1d88f02b0587
naman <naman.gupta_cs18@gla.ac.in>
2020-03-27 20:32:36 +0530

    making object of Scanner class

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,8 +7,12 @@
 package problem1.main;
 // executable class
 
+import java.util.Scanner;
+
 public class MyMain {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
 
     }
 }
