61b741a4cd03362536419bf39c136d48dcfbb9fc
naman <naman.gupta_cs18@gla.ac.in>
2020-03-27 20:39:13 +0530

    adding main functionality

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,11 +7,28 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
+
 import java.util.Scanner;
 
 public class MyMain {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        MyBinarySearchTree bst = new MyBinarySearchTree();
+        System.out.println("Enter number of nodes");/*-->*/
+        int n = sc.nextInt();
+        while (n-- > 0) {
+            System.out.println("Enter the data");
+            int data = sc.nextInt();
+            TreeNode node = new TreeNode();
+            node.setData(data);
+            bst.insert(node);
+        }
+        System.out.println(bst.showLeft(bst.getRoot(), false));
+
+
+
 
 
     }
