c1ecb3a445c9c73d5acb5a5c9fd174d216ce887a
naman <naman.gupta_cs18@gla.ac.in>
2020-03-27 20:46:26 +0530

    importing TreeNode class

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -7,6 +7,7 @@
 package problem2.main;
 
 import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
 
 import java.util.Scanner;
 
@@ -16,6 +17,15 @@ public class MyMain {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         MyBinarySearchTree bst = new MyBinarySearchTree();
+        System.out.println("Enter the number of nodes:");
+        int noOfNodes = sc.nextInt();
+        while (noOfNodes-- > 0) {
+            System.out.println("Enter data");/*-->*/
+            int val = sc.nextInt();
+            TreeNode node = new TreeNode();
+            node.setData(val);
+            bst.insert(node);
+        }
 
     }
 }
