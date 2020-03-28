14d4f5a2affde94cedfe5dc5490b939065c32579
naman <naman.gupta_cs18@gla.ac.in>
2020-03-28 13:55:50 +0530

    creating object of MyCircularQueue class

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -6,12 +6,15 @@
  */
 package problem5.main;
 
+import problem5.circularqueue.MyCircularQueue;
+
 import java.util.Scanner;
 
 //executable class
 public class MyMain {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        MyCircularQueue cq = new MyCircularQueue();
 
     }
 }
