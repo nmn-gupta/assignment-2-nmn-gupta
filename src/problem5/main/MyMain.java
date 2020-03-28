91698883dd6c171039482f1db7b86d737acc6fd0
naman <naman.gupta_cs18@gla.ac.in>
2020-03-28 13:55:14 +0530

    creating object of Scanner class

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -5,9 +5,13 @@
  *  Time: 7:06 PM
  */
 package problem5.main;
+
+import java.util.Scanner;
+
 //executable class
 public class MyMain {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
     }
 }
