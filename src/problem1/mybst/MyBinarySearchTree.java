b30af3e7834b34acf137f7c49abb1de63304e265
naman <naman.gupta_cs18@gla.ac.in>
2020-03-28 14:49:11 +0530

    update MyBinarySearchtTree class

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -9,8 +9,6 @@ package problem1.mybst;
 import problem1.node.TreeNode;
 
 import java.util.ArrayList;
-
-// to implement BinarySearchTree
 public class MyBinarySearchTree {
     private TreeNode root;
     private ArrayList<Integer> arrayList;
