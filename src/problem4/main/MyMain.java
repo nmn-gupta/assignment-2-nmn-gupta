471205a28e3c4f9401ac7ea7dfc630c3decd8b27
naman <naman.gupta_cs18@gla.ac.in>
2020-03-28 12:35:25 +0530

    creating object of MyQueue class

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -7,6 +7,7 @@
 package problem4.main;
 
 import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
 
 import java.util.Scanner;
 
@@ -16,6 +17,9 @@ public class MyMain {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         MyBinarySearchTree bst = new MyBinarySearchTree();
+        MyQueue queue = new MyQueue();
+
+
 
     }
 }
