6b74d7a217c0e77be5d26f0a08de62b3fd3e661d
naman <naman.gupta_cs18@gla.ac.in>
2020-03-28 12:08:19 +0530

    creating object of MyPriorityQueue class

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -6,6 +6,8 @@
  */
 package problem3.main;
 
+import problem3.myqueue.MyPriorityQueue;
+
 import java.util.Scanner;
 
 // executable class
@@ -13,6 +15,7 @@ import java.util.Scanner;
 public class MyMain {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        MyPriorityQueue pq = new MyPriorityQueue();
 
 
     }
