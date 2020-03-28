0814eb3bbe23f05849924671f130d1aeec1b8eab
naman <naman.gupta_cs18@gla.ac.in>
2020-03-28 12:33:59 +0530

    creating object of Scanner class

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -5,11 +5,14 @@
  *  Time: 7:08 PM
  */
 package problem4.main;
+
+import java.util.Scanner;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
     }
 }
 
