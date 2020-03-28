5c3f00dbc1ad3284e2b11452dd9f72319f8e825a
naman <naman.gupta_cs18@gla.ac.in>
2020-03-28 12:34:42 +0530

    creating object of MyBinarySearchTree class

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -6,6 +6,8 @@
  */
 package problem4.main;
 
+import problem1.mybst.MyBinarySearchTree;
+
 import java.util.Scanner;
 
 // executable class
@@ -13,6 +15,8 @@ import java.util.Scanner;
 public class MyMain {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        MyBinarySearchTree bst = new MyBinarySearchTree();
+
     }
 }
 
