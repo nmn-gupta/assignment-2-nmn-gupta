34c712805093da041af86e87338a845e0d552a49
naman <naman.gupta_cs18@gla.ac.in>
2020-03-28 13:01:44 +0530

    generate getter the setter methods for name field

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -11,4 +11,11 @@ public class Student {
     private int rollNum;
     private int backLogCounter;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
