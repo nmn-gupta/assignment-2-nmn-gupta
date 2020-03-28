774e309e68924fc1c4edd308f374eae4494ce1c2
naman <naman.gupta_cs18@gla.ac.in>
2020-03-28 12:19:21 +0530

    generate getter and stter methods for data field

--- src/problem4/node/Node.java
+++ src/problem4/node/Node.java
@@ -4,4 +4,11 @@ public class Node {
     private int data;
     private Node next;
 
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
 }
