f2c344f0e9dc05db57a428ab0b4d23d563c984b3
naman <naman.gupta_cs18@gla.ac.in>
2020-03-28 12:07:27 +0530

    creating object of Scanner class

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,10 +5,15 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import java.util.Scanner;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
 
     }
 }
