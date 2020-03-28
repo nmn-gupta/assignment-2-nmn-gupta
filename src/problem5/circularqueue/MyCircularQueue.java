c467e58f23be2b643ad2af30529710e397ec2257
naman <naman.gupta_cs18@gla.ac.in>
2020-03-28 13:48:05 +0530

    adding deQueue method

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -52,5 +52,21 @@ public class MyCircularQueue {
         }
     }
 
+    public Node deQueue() {
+        Node temp;
+        if (getFront() == null) {
+            return null;
+        } else if (getFront() == getRear()) {
+            temp = getFront();
+            setRear(null);
+            setFront(null);
+        } else {
+            temp = getFront();
+            setFront(getFront().getNext());
+            getRear().setNext(getFront());
+        }
+        return temp;
+    }
+
 }
 
