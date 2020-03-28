251ce35e2b72c8160a3e827ed9964f45584a6f4f
naman <naman.gupta_cs18@gla.ac.in>
2020-03-28 12:32:58 +0530

    adding main method

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -8,4 +8,8 @@ package problem4.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+
+    }
 }
+
